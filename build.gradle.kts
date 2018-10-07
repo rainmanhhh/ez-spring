7bb5157042be29f70da318aa5ee349a8e469e8c6
huangwl <410488219@qq.com>
2018-10-07 13:36:12 +0800

    add javadoc & sources to artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,12 +6,11 @@ plugins {
     maven
     kotlin("jvm") version v.kotlin
     id("org.jetbrains.kotlin.plugin.spring") version v.kotlin
-//    id("org.springframework.boot") version v.springboot
     id("io.spring.dependency-management") version v.springDepManage
 }
 
 group = "ez"
-version = "0.0.1"
+version = "0.0.3"
 
 repositories {
     mavenLocal()
@@ -29,4 +28,22 @@ dependencies {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+val sourceJar by tasks.creating(Jar::class) {
+    classifier = "sources"
+    val main by sourceSets
+    from(main.allSource)
+}
+
+val javadoc by tasks
+val javadocJar by tasks.creating(Jar::class) {
+    classifier = "javadoc"
+    from(javadoc)
+}
+
+artifacts {
+    val configName = "archives"
+    add(configName, sourceJar)
+    add(configName, javadocJar)
 }
