293ceeea78944ae86ce88419a58345df33335850
huangwl <410488219@qq.com>
2018-11-10 17:28:06 +0800

    improve jooq ex

--- src/main/kotlin/ez/db/dsl/EzDsl.kt
+++ src/main/kotlin/ez/db/dsl/EzDsl.kt
@@ -23,19 +23,4 @@ object EzDsl {
         if (it.isEmpty()) null
         else DSL.or(it)
     }
-
-    /**
-     *
-     * @param expr Any? check expr like javascript `!!`
-     * @return Boolean false if expr is `null` or "" or `false`; otherwise true
-     */
-    fun checkAsJs(expr: Any?): Boolean = expr != null && expr != "" && expr != false
-
-    /**
-     * if [checkAsJs] ([expr]) returns true, use [provider] to create [Condition]; else return null
-     * @param expr E?
-     * @param provider (E) -> Condition
-     * @return Condition?
-     */
-    fun <E : Any> condition(expr: E?, provider: (E) -> Condition) = if (EzDsl.checkAsJs(expr)) provider(expr!!) else null
 }
--- src/main/kotlin/ez/db/step/SelectConditionStepEx.kt
+++ src/main/kotlin/ez/db/step/SelectConditionStepEx.kt
@@ -1,18 +1,21 @@
 package ez.db.step
 
-import ez.db.dsl.EzDsl
-import org.jooq.Condition
-import org.jooq.Record
-import org.jooq.SelectConditionStep
+import org.jooq.*
+import org.jooq.impl.DSL
 
-fun <RECORD : Record> SelectConditionStep<RECORD>.ezAnd(
-    vararg conditions: Condition?
-): SelectConditionStep<RECORD> = EzDsl.and(*conditions).let {
-    if (it == null) this else and(it)
+fun Condition.and(expr: Boolean, provider: () -> Condition) =
+    if (expr) and(provider()) else this
+
+fun Condition.or(expr: Boolean, provider: () -> Condition) =
+    if (expr) or(provider()) else this
+
+fun Condition.and(action: True.() -> Condition): Condition = DSL.trueCondition().run(action).let {
+    if (it == DSL.trueCondition()) this else and(it)
 }
 
-fun <RECORD : Record> SelectConditionStep<RECORD>.ezOr(
-    vararg conditions: Condition?
-): SelectConditionStep<RECORD> = EzDsl.or(*conditions).let {
-    if (it == null) this else or(it)
+fun Condition.or(action: False.() -> Condition): Condition = DSL.falseCondition().run(action).let {
+    if (it == DSL.falseCondition()) this else or(it)
 }
+
+fun <R : Record> SelectWhereStep<R>.where(builder: True.() -> Condition) =
+    where(DSL.trueCondition().run(builder))
